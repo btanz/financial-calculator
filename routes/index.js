8ecd6292a81506ace75c0d27840b41c6722584a1
Benjamin Tanz <btanz@gmx.de>
2015-06-09 15:18:20 +0200

    Added Materialize and created layout

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {title: 'Express', appTitle: 'SimplyFi'});
+});
+
+
+router.get('/ben', function(req, res, next) {
+  res.render('index', { title: 'Ben' });
 });
 
 module.exports = router;
