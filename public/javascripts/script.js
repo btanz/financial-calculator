3c6385b5afb4d8c8aeea1a67170acf605b6f0579
Benjamin Tanz <btanz@gmx.de>
2015-06-10 18:24:49 +0200

    Continued working on Optionspreisrechner

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,7 @@
 (function($){
   $(function(){
     $('.button-collapse').sideNav({'edge': 'left'});
+    $('select').material_select();
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
