a2a8ad0bfc056f72ec2b33837f8d596491939e63
Benjamin Tanz <btanz@gmx.de>
2015-06-14 18:00:00 +0200

    Minor changes

--- public/js/app.controller.js
+++ public/js/app.controller.js
@@ -16,21 +16,6 @@ app.controller = (function() {
     // initialize materializecss tooltips
     $('.tooltipped').tooltip({delay: 50});
 
-    // initialize materializecss datepicker XXX abstract
-    $('.datepicker').pickadate({
-      monthsFull: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
-      weekdaysShort: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
-      today: 'heute',
-      clear: 'löschen',
-      close: 'fertig',
-      formatSubmit: 'yyyy/mm/dd',
-
-      selectMonths: true, // Creates a dropdown to control month
-      selectYears: 15 // Creates a dropdown of 15 years to control year
-    });
-
-    //XXX Temp
-    $('.modal-trigger').leanModal();
 
     // run initialization steps
     app.init();
