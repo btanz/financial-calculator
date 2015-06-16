df2fde732b7a0164164d57e64f65e3473df0bb1c
Benjamin Tanz <btanz@gmx.de>
2015-06-16 08:51:54 +0200

    Fixed on Aktienrenditerechner

--- public/js/app.boerse.js
+++ public/js/app.boerse.js
@@ -3,15 +3,24 @@ app.boerse = (function() {
   /*********************** BEGIN BOERSE DOCUMENT READY TASKS ***********************/
   $(document).ready(function () {
 
+    // attach a div where dividend dates and amounts will be added dynamically
+    $('#boerse-equityreturn-dividends').closest('div[class^="form-group"]').after('<div class="dividendsInput"></div>');
+
+    // attach event handler for dividend input fields
+    $('#boerse-equityreturn-dividends').on('change', function (e) {
+      toggleInputsDividends(e);
+    });
+
+    // attach event handler for fee inputs
     $('#boerse-equityreturn-fees').on('change', function (e) {
-      toggleInputs(e);
+      toggleInputsFees(e);
     });
 
   });
   /*********************** END BOERSE DOCUMENT READY TASKS *************************/
 
   /*********************** BEGIN BOERSE EVENT HANDLERS ***********************/
-  function toggleInputs (e){
+  function toggleInputsFees (e){
     e.preventDefault();
     var state = parseInt($('#boerse-equityreturn-fees').val());
     if (state === 2){
@@ -19,8 +28,32 @@ app.boerse = (function() {
     } else if (state === 1){
       $('#boerse-equityreturn-feebuy, #boerse-equityreturn-feesell').closest('div[class^="form-group"]').addClass('hide')
     }
-
   }
+
+  function toggleInputsDividends(e){
+    e.preventDefault();
+    var state = parseInt($('#boerse-equityreturn-dividends').val());
+    if (state !== 0){
+      $('.dividendsInput').children().remove();
+      for(var i = 1; i<= state; i++){
+        app.helpers.compileTemplate('.dividendsInput','#boerse-equityreturn-dividendsInput-template', {count: i + '. Dividende', id1: 'boerse-equityreturn-dividendDate' + i, id2: 'boerse-equityreturn-dividendAmount'+i}, true);
+      }
+      // initialize datepicker
+      $('.input-group.date').datepicker({
+        language: 'de',
+        todayBtn: "linked",
+        keyboardNavigation: false,
+        forceParse: false,
+        calendarWeeks: true,
+        autoclose: true
+      });
+    } else {
+      $('.dividendsInput').children().remove();
+    }
+  }
+
+
+
 
   /*********************** END BOERSE EVENT HANDLERS ***********************/
 
--- public/js/app.helpers.js
+++ public/js/app.helpers.js
@@ -8,12 +8,14 @@ app.helpers = (function($) {
   var compileTemplate, numeralInit;
 
   // This function reads the Handlebars template with templateId and compiles it to html using the dataObj
-  // Then, it removes all childs from the containerId and appends the compiled template as child to containerId
-  compileTemplate = function(containerSelector,templateSelector,dataObj){
+  // Then, it removes all childs (optionally keeps childs) from the containerId and appends the compiled template as child to containerId
+  compileTemplate = function(containerSelector,templateSelector,dataObj, keepChilds){
     var templateSource = $(templateSelector).text();
     var template = Handlebars.compile(templateSource);
     var HTML = template({obj: dataObj});
-    $(containerSelector).children().remove();
+    if (!keepChilds){
+      $(containerSelector).children().remove();
+    }
     $(containerSelector).append(HTML);
   };
 
