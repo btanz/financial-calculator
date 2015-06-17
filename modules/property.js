8d271a35161f1fa9b56e7db550056506d17b4fef
Benjamin Tanz <btanz@gmx.de>
2015-06-17 16:54:53 +0200

    Finished immobilienrenditerechner

--- modules/property.js
+++ modules/property.js
@@ -25,7 +25,8 @@ exports.propertyreturn = function(inputs){
 
   /* ******** 1. INIT AND ASSIGN ******** */
   var revenuePV, goal, irr, helper = {};
-  var result = {}; result._1 = {};
+  var cash = [], totals = [], cashHelper;
+  var result = {}; result._1 = {}; result._2 = {};
   var localElems = calcElems.propertyreturn.outputs;
   var expectedInputs = calcElems.propertyreturn.inputs;
   var errorMap;
@@ -76,23 +77,16 @@ exports.propertyreturn = function(inputs){
   // find root of goal function
   irr = (math.roots(goal,0.5)) * 100;
 
-
-
-
-
   // sanitize result
   if (!irr || (isNaN(irr))){
     return null;
   }
 
-
-
-
   // assign result to return object
   helper.irr = irr;
 
-  // *** 3.B. Compute aggregate revenues and costs ***
 
+  // *** 3.B. Compute aggregate revenues and costs ***
   // todo: calculations should be alright, but re-very once monthly values are computed
   helper.revenue = inputs.revDynamic !== 0 ? inputs.terminal + inputs.revenue * 12 * (1-Math.pow(1 + inputs.revDynamic, Math.floor(inputs.term))) / (-inputs.revDynamic) + (Math.ceil((inputs.term - Math.floor(inputs.term)) * 12)) * inputs.revenue * Math.pow(1+inputs.revDynamic,Math.floor(inputs.term)) : inputs.terminal + (Math.ceil(inputs.term * 12)) * inputs.revenue;
   helper.rentRevenue = helper.revenue - inputs.terminal;
@@ -101,19 +95,71 @@ exports.propertyreturn = function(inputs){
   helper.investment = inputs.equity + (Math.ceil(inputs.termLoan * 12)) * inputs.repay + helper.maintenance;
   helper.loan =  (Math.ceil(inputs.termLoan * 12)) * inputs.repay;
   helper.initialInvestment = inputs.equity;
-  helper.profit = helper.revenue - inputs.equity;
+  helper.profit = helper.revenue - inputs.equity - (Math.ceil(inputs.termLoan * 12)) * inputs.repay - helper.maintenance;
+
+
+  // *** 3.C. Compute monthly and annual values  / all values b.o.p ***
+  cash[0] = []; cash[1] = []; cash[2] = []; cash[3] = []; cash[4] = []; cash[5] = []; cash[6] = [];
+  cash[0][0] = 1;
+  cash[1][0] = inputs.maintenance;
+  cashHelper = inputs.termLoan !== 0 ? inputs.repay : 0;
+  cash[2][0] = (inputs.term > 1) ? cash[1][0] * 12 : cash[1][0] * Math.ceil(12 * (inputs.term));
+  cash[2][0] += (inputs.termLoan > 1) ? cashHelper * 12 : cashHelper * Math.ceil(12 * (inputs.termLoan));
+  cash[1][0] += cashHelper;
+  totals[0] = cash[2][0];
+  cash[3][0] = inputs.revenue;
+  cash[4][0] = (inputs.term > 1) ? cash[3][0] * 12 : cash[3][0] * Math.ceil(12 * (inputs.term));
+  totals[1] = cash[4][0];
+  cash[5][0] = cash[4][0] - cash[2][0];
+  cash[6][0] = - inputs.equity + cash[5][0];
 
 
 
+  for (var i=1; i < inputs.term; i++){
+    cash[0][i] = i + 1;
+    cash[1][i] = inputs.maintenance * Math.pow(1 + inputs.costDynamic,i);
+    cash[2][i] = (inputs.term - i > 1) ? cash[1][i] * 12 : cash[1][i] * Math.ceil(12 * (inputs.term - i));
+    cashHelper = i < inputs.termLoan ? inputs.repay : 0;
+    cash[2][i] += (inputs.termLoan - i > 1) ? cashHelper * 12 : cashHelper * Math.ceil(12 * (inputs.termLoan - i));
+    cash[1][i] += cashHelper;
+    cash[3][i] = inputs.revenue * Math.pow(1 + inputs.revDynamic,i);
+    cash[4][i] = (inputs.term - i > 1) ? cash[3][i] * 12 : cash[3][i] * Math.ceil(12 * (inputs.term - i));
+    cash[5][i] = cash[4][i] - cash[2][i];
+    cash[6][i] = cash[6][i-1] + cash[5][i];
+    totals[0] += cash[2][i]; totals[1] += cash[4][i];
+  }
+  totals[2] = - inputs.equity - totals[0] + totals[1] + inputs.terminal;
+
+  // transpose cash array
+  helper.cash = cash[0].map(function(col,i){
+    return cash.map(function(row){
+      return row[i];
+    })
+  });
 
   /* ******** 4. CONSTRUCT RESULT OBJECT ******** */
+
+  // first result container
   result.id = calcElems.propertyreturn.id;
-  result._1.irr = {'description': 'Rendite (Effektivzins, IRR)',  'value': helper.irr,             'unit': '% p. a.', 'digits': 3, 'tooltip': {}};
+  result._1.irr =         {'description': 'Rendite (Effektivzins, IRR)',  'value': helper.irr,             'unit': '% p. a.',  'digits': 3,  'tooltip': {}};
+  result._1.profit =      {'description': 'Gewinn',                       'value': helper.profit,          'unit': 'EUR',      'digits': 2,  'tooltip': {}};
+  result._1.revenue =     {'description': 'Gesamte Einnahmen',            'value': helper.revenue,         'unit': 'EUR',      'digits': 2,  'tooltip': {}};
+  result._1.rentRevenue = {'description': 'Monatliche Mieteinnahmen',     'value': helper.rentRevenue,     'unit': 'EUR',      'digits': 2,  'tooltip': {}};
+  result._1.sellRevenue = {'description': 'Endwert',                      'value': helper.sellRevenue,     'unit': 'EUR',      'digits': 2,  'tooltip': {}};
+  result._1.investment =  {'description':  'Gesamte Ausgaben',            'value': helper.investment,      'unit': 'EUR',      'digits': 2,  'tooltip': {}};
+  result._1.initialInvestment = {'description':  'Anfangsinvestition',    'value': helper.initialInvestment,'unit': 'EUR',     'digits': 2,  'tooltip': {}};
+  result._1.maintenance = {'description':  'Instandhaltungskosten',       'value': helper.maintenance,     'unit': 'EUR',      'digits': 2,  'tooltip': {}};
+  result._1.loan =        {'description':  'Darlehenszahlungen',          'value': helper.loan,            'unit': 'EUR',      'digits': 2,  'tooltip': {}};
 
-
+  // second result container
+  result._2.title = 'Zahlungsübersicht';
+  result._2.header = ['Jahr', 'Kosten', 'Einnahmen', 'Gewinn'];
+  result._2.headersub = ['Mon.', 'Jahr', 'Mon.', 'Jahr', 'Jahr', 'kumuliert'];
+  result._2.firstrow = ['Anfangsinvestition von ', ' EUR', helper.initialInvestment, -helper.initialInvestment];
+  result._2.lastrow = ['Endwert von ', ' EUR', helper.sellRevenue, helper.sellRevenue + cash[6][cash[6].length-1]];
+  result._2.body = helper.cash;
+  result._2.totalsrow = ['&sum;','',totals[0],'',totals[1],totals[2],totals[2]];
 
   return result;
 };
-
-
 
