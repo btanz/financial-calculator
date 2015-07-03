3744c49658546948ca128e45c1a8c6a518a14417
Benjamin Tanz <btanz@gmx.de>
2015-07-03 08:46:08 +0200

    Added a lib directory, moved finance module there, designed tests for finance module

---
+++ test/test_spec.js
@@ -0,0 +1,9 @@
+var assert = require("assert");
+
+describe("Test suite test",function(){
+  describe("The test suite is working",function(){
+    it("can process this test", function(){
+      assert(true);
+    });
+  });
+});
