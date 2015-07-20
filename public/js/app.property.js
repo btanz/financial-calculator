de7f93c2f6ff66dc03d308485b1136c5b78ddaa2
Benjamin Tanz <btanz@gmx.de>
2015-07-20 18:24:33 +0200

    Continued working on property-mortgage input fields

--- public/js/app.property.js
+++ public/js/app.property.js
@@ -24,10 +24,115 @@ app.property = (function() {
     });
 
 
+    // attach event handler for fees
+    $('#property-mortgage-fees').on('change', function (e) {
+      toggleMortgageInputsFees(e);
+    });
+
+
+    // attach event handler for disagio
+    $('#property-mortgage-disagio').on('change', function (e) {
+      toggleMortgageInputsDisagio(e);
+    });
+
+    // attach event handler for repaymentfree
+    $('#property-mortgage-repaymentfree').on('change', function (e) {
+      toggleMortgageInputsRepaymentfree(e);
+    });
+
+    // attach event handler for followup
+    $('#property-mortgage-followup').on('change', function (e) {
+      toggleMortgageInputsFollowup(e);
+    });
+
+    // attach event handler for specialrepay
+    $('#property-mortgage-specialrepay').on('change', function (e) {
+      toggleMortgageInputsSpecialrepay(e);
+    });
+
+    // attach a div where specialrepay times and amounts will be added dynamically
+    $('#property-mortgage-specialrepaypositions').closest('div[class^="form-group"]').after('<div class="specialrepayInput"></div>');
+
+    // attach event handler for specialrepaypositions input fields
+    $('#property-mortgage-specialrepaypositions').on('change', function (e) {
+      toggleMortgageInputsSpecialrepaypositions(e);
+    });
+
+
   });
   /*********************** END PROPERTY DOCUMENT READY TASKS *************************/
 
   /*********************** BEGIN PROPERTY EVENT HANDLERS ***********************/
+  function toggleMortgageInputsFees (e){
+    e.preventDefault();
+    var state = $('#property-mortgage-fees').val();
+    if (state === 'true'){
+      $('#property-mortgage-feeamount, #property-mortgage-feetype').closest('div[class^="form-group"]').removeClass('hide')
+    } else if (state === 'false'){
+      $('#property-mortgage-feeamount, #property-mortgage-feetype').closest('div[class^="form-group"]').addClass('hide')
+    }
+  }
+
+
+  function toggleMortgageInputsDisagio (e){
+    e.preventDefault();
+    var state = $('#property-mortgage-disagio').val();
+    if (state === 'true'){
+      $('#property-mortgage-disagioamount').closest('div[class^="form-group"]').removeClass('hide')
+    } else if (state === 'false'){
+      $('#property-mortgage-disagioamount').closest('div[class^="form-group"]').addClass('hide')
+    }
+  }
+
+  function toggleMortgageInputsFollowup (e){
+    e.preventDefault();
+    var state = $('#property-mortgage-followup').val();
+    if (state === 'true'){
+      $('#property-mortgage-followupinterest').closest('div[class^="form-group"]').removeClass('hide')
+    } else if (state === 'false'){
+      $('#property-mortgage-followupinterest').closest('div[class^="form-group"]').addClass('hide')
+    }
+  }
+
+  function toggleMortgageInputsRepaymentfree (e){
+    e.preventDefault();
+    var state = $('#property-mortgage-repaymentfree').val();
+    if (state === 'true'){
+      $('#property-mortgage-repaymentfreeterm, #property-mortgage-repaymentfreetype').closest('div[class^="form-group"]').removeClass('hide')
+    } else if (state === 'false'){
+      $('#property-mortgage-repaymentfreeterm, #property-mortgage-repaymentfreetype').closest('div[class^="form-group"]').addClass('hide')
+    }
+  }
+
+  function toggleMortgageInputsSpecialrepay (e){
+    e.preventDefault();
+    var state = $('#property-mortgage-specialrepay').val();
+    if (state === 'true'){
+      $('#property-mortgage-specialrepaypositions').closest('div[class^="form-group"]').removeClass('hide');
+    } else if (state === 'false'){
+      $('#property-mortgage-specialrepaypositions').closest('div[class^="form-group"]').addClass('hide');
+      $('.specialrepayInput').children().remove();
+      $('#property-mortgage-specialrepaypositions').val(0);
+    }
+  }
+
+
+  function toggleMortgageInputsSpecialrepaypositions(e){
+    e.preventDefault();
+    var state = parseInt($('#property-mortgage-specialrepaypositions').val());
+    var first = false;
+    if (state !== 0){
+      $('.specialrepayInput').children().remove();
+      for(var i = 0; i< state; i++){
+        if (i === 0) {first = true} else {first = false};
+        app.helpers.compileTemplate('.specialrepayInput','#property-mortgage-specialrepayInput-template', {count: String(i+1) + '. Tilgung', id1: 'property-mortgage-specialrepaymonths' + i, id2: 'property-mortgage-specialrepayamount'+i, first: first}, true);
+      }
+    } else {
+      $('.specialrepayInput').children().remove();
+    }
+  }
+
+
   function toggleInputsDynamics(e){
     e.preventDefault();
     var state = $('#property-buyrent-dynamics').val();
