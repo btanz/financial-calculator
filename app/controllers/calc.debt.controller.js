c528e0d2e4b8d97e58d8afb4f238c96a9db9e0d1
Benjamin Tanz <btanz@gmx.de>
2015-08-11 09:40:36 +0200

    minor changes on debt-annuity

--- app/controllers/calc.debt.controller.js
+++ app/controllers/calc.debt.controller.js
@@ -5,15 +5,36 @@ var debt = require('../modules/debt');
 /** calculator-debt-annuity */
 exports.annuity = {
 
-  render: function(req,res,next){
-    res.render('calculator', {obj: calcElems.annuity});
+  render: function(req, res) {
+    var Calc = require('mongoose').model('Calc');
+
+    Calc.findByCalcname('annuity')
+        .then(function(data){
+          res.render('calculator', {obj: data[0]});
+        })
+        .onReject(function(){
+          console.log("An error occurred while rendering the boerse-options calculator.");
+        });
   },
 
+  calculate: function(req, res){
+    var obj = req.query;
+    debt.annuity(obj)
+        .then(function(results){
+          res.json(results);
+        })
+        .onReject(function(){
+          console.log('Error occurred');
+          res.json({});
+        });
+  }
+
+/*
   calculate: function(req,res,next){
     var obj = req.query;
     var results = debt.annuity(obj);
     res.json(results);
-  }
+  }*/
 };
 
 
