b9c323222d21494fb28e7aee270b5115907d5f91
Benjamin Tanz <btanz@gmx.de>
2015-09-15 09:46:23 +0200

    tests for dispo

--- test/debt_spec.js
+++ test/debt_spec.js
@@ -13,7 +13,7 @@ var ROUND_PRECISION = 100;   // 1/100 rounding precision
 /** run tests */
 describe("Debt calculators are correct", function() {
 
-  describe("Property-repaysurrogat correct", function() {
+  describe("Debt-repaysurrogat correct", function() {
     var data = [],
         expectations = [];
     before(function () {
@@ -233,6 +233,246 @@ describe("Debt calculators are correct", function() {
 
     it('Passes 20th test set', function(done){
       debt.repaysurrogat(data[19]).then(function(results){
+        var values = {};
+        _.each(results._1, function(el, ind, list){ values[ind]= Math.round(el.value * ROUND_PRECISION) / ROUND_PRECISION;});
+        assert(_.isMatch(values, expectations[19]));
+        done();
+      }).onReject(done);
+    });
+
+  });
+
+
+
+
+
+
+  describe("debt-dispo correct", function() {
+    var data = [],
+        expectations = [];
+    before(function () {
+
+      data[0]  = {principal: '500', limit: '1000', dispointerest: '10.5', otherinterest: '15.5', periodchoice: 'days', days: '100', startdate: '', enddate: '', daycount: 'a30E360'};
+      data[1]  = {principal: '159.74', limit: '839.45', dispointerest: '0.06', otherinterest: '1.69', periodchoice: 'days', days: '175', startdate: '', enddate: '', daycount: 'act360'};
+      data[2]  = {principal: '29.15', limit: '155.63', dispointerest: '5.4', otherinterest: '14.69', periodchoice: 'days', days: '91', startdate: '', enddate: '', daycount: 'act365'};
+      data[3]  = {principal: '711.81', limit: '887.81', dispointerest: '0.7', otherinterest: '2.73', periodchoice: 'days', days: '153', startdate: '', enddate: '', daycount: 'actact'};
+      data[4]  = {principal: '196.07', limit: '680.79', dispointerest: '3.69', otherinterest: '10.35', periodchoice: 'days', days: '117', startdate: '', enddate: '', daycount: 'a30E360'};
+      data[5]  = {principal: '660.28', limit: '325.13', dispointerest: '8.4', otherinterest: '17.61', periodchoice: 'days', days: '118', startdate: '', enddate: '', daycount: 'act360'};
+      data[6]  = {principal: '173.56', limit: '582.02', dispointerest: '4.55', otherinterest: '9.19', periodchoice: 'days', days: '84', startdate: '', enddate: '', daycount: 'act365'};
+      data[7]  = {principal: '155.97', limit: '745.15', dispointerest: '4.64', otherinterest: '8.52', periodchoice: 'days', days: '45', startdate: '', enddate: '', daycount: 'actact'};
+      data[8]  = {principal: '837.78', limit: '958.19', dispointerest: '1.71', otherinterest: '7.42', periodchoice: 'days', days: '142', startdate: '', enddate: '', daycount: 'a30E360'};
+      data[9]  = {principal: '170.27', limit: '866.52', dispointerest: '9', otherinterest: '11.56', periodchoice: 'days', days: '141', startdate: '', enddate: '', daycount: 'act360'};
+      data[10] = {principal: '263.66', limit: '289.46', dispointerest: '5.33', otherinterest: '5.76', periodchoice: 'dates', days: '', startdate: '31.12.2006', enddate: '24.06.2015', daycount: 'act365'};
+      data[11] = {principal: '91.63', limit: '658.32', dispointerest: '4.41', otherinterest: '11.71', periodchoice: 'dates', days: '', startdate: '03.01.2008', enddate: '29.01.2011', daycount: 'actact'};
+      data[12] = {principal: '691.63', limit: '973.5', dispointerest: '4.39', otherinterest: '8.05', periodchoice: 'dates', days: '', startdate: '06.01.2008',  enddate: '07.03.2011', daycount: 'a30E360'};
+      data[13] = {principal: '630.64', limit: '783.57', dispointerest: '2.76', otherinterest: '12.41', periodchoice: 'dates', days: '', startdate: '18.01.2008',enddate: '27.03.2011', daycount: 'act360'};
+      data[14] = {principal: '529.12', limit: '700.12', dispointerest: '6.59', otherinterest: '14.56', periodchoice: 'dates', days: '', startdate: '17.02.2008',enddate: '16.05.2011', daycount: 'act365'};
+      data[15] = {principal: '866.6', limit: '265.57', dispointerest: '1.99', otherinterest: '10.21', periodchoice: 'dates', days: '', startdate: '06.03.2008', enddate: '30.06.2011', daycount: 'actact'};
+      data[16] = {principal: '770.42', limit: '966.78', dispointerest: '4.98', otherinterest: '12.43', periodchoice: 'dates', days: '', startdate: '13.03.2008',enddate: '06.07.2011', daycount: 'a30E360'};
+      data[17] = {principal: '187.15', limit: '432.86', dispointerest: '5.5', otherinterest: '12.04', periodchoice: 'dates', days: '', startdate: '20.03.2008', enddate: '14.07.2011', daycount: 'act360'};
+      data[18] = {principal: '973.66', limit: '689.89', dispointerest: '8.83', otherinterest: '9.36', periodchoice: 'dates', days: '', startdate: '04.04.2008', enddate: '15.07.2011', daycount: 'act365'};
+      data[19] = {principal: '868.55', limit: '870.55', dispointerest: '4.66', otherinterest: '5.54', periodchoice: 'dates', days: '', startdate: '11.04.2008', enddate: '27.09.2011', daycount: 'actact'};
+
+
+      expectations[0] = {interestamount: 14.58, averageinterest: 10.5};
+      expectations[1] = {interestamount: 0.05, averageinterest: 0.06};
+      expectations[2] = {interestamount: 0.39, averageinterest: 5.4};
+      expectations[3] = {interestamount: 2.09, averageinterest: 0.7};
+      expectations[4] = {interestamount: 2.35, averageinterest: 3.69};
+      expectations[5] = {interestamount: 28.30, averageinterest: 13.07};
+      expectations[6] = {interestamount: 1.82, averageinterest: 4.55};
+      expectations[7] = {interestamount: 0.89, averageinterest: 4.64};
+      expectations[8] = {interestamount: 5.65, averageinterest: 1.71};
+      expectations[9] = {interestamount: 6.00, averageinterest: 9};
+      expectations[10] = {interestamount: 119.24, averageinterest: 5.33};
+      expectations[11] = {interestamount: 12.41, averageinterest: 4.41};
+      expectations[12] = {interestamount: 96.23, averageinterest: 4.39};
+      expectations[13] = {interestamount: 56.28, averageinterest: 2.76};
+      expectations[14] = {interestamount: 113.11, averageinterest: 6.59};
+      expectations[15] = {interestamount: 220.98, averageinterest: 7.69};
+      expectations[16] = {interestamount: 127.14, averageinterest: 4.98};
+      expectations[17] = {interestamount: 34.63, averageinterest: 5.5};
+      expectations[18] = {interestamount: 286.88, averageinterest: 8.98};
+      expectations[19] = {interestamount: 140.07, averageinterest: 4.66};
+
+    });
+
+
+    it('Passes 1st test set', function(done){
+      debt.dispo(data[0]).then(function(results){
+        var values = {};
+        _.each(results._1, function(el, ind, list){ values[ind]= Math.round(el.value * ROUND_PRECISION) / ROUND_PRECISION;});
+        assert(_.isMatch(values, expectations[0]));
+        done();
+      }).onReject(done);
+    });
+
+
+    it('Passes 2nd test set', function(done){
+      debt.dispo(data[1]).then(function(results){
+        var values = {};
+        _.each(results._1, function(el, ind, list){ values[ind]= Math.round(el.value * ROUND_PRECISION) / ROUND_PRECISION;});
+        assert(_.isMatch(values, expectations[1]));
+        done();
+      }).onReject(done);
+    });
+
+
+    it('Passes 3rd test set', function(done){
+      debt.dispo(data[2]).then(function(results){
+        var values = {};
+        _.each(results._1, function(el, ind, list){ values[ind]= Math.round(el.value * ROUND_PRECISION) / ROUND_PRECISION;});
+        assert(_.isMatch(values, expectations[2]));
+        done();
+      }).onReject(done);
+    });
+
+    it('Passes 4th test set', function(done){
+      debt.dispo(data[3]).then(function(results){
+        var values = {};
+        _.each(results._1, function(el, ind, list){ values[ind]= Math.round(el.value * ROUND_PRECISION) / ROUND_PRECISION;});
+        assert(_.isMatch(values, expectations[3]));
+        done();
+      }).onReject(done);
+    });
+
+    it('Passes 5th test set', function(done){
+      debt.dispo(data[4]).then(function(results){
+        var values = {};
+        _.each(results._1, function(el, ind, list){ values[ind]= Math.round(el.value * ROUND_PRECISION) / ROUND_PRECISION;});
+        assert(_.isMatch(values, expectations[4]));
+        done();
+      }).onReject(done);
+    });
+
+    it('Passes 6th test set', function(done){
+      debt.dispo(data[5]).then(function(results){
+        var values = {};
+        _.each(results._1, function(el, ind, list){ values[ind]= Math.round(el.value * ROUND_PRECISION) / ROUND_PRECISION;});
+        assert(_.isMatch(values, expectations[5]));
+        done();
+      }).onReject(done);
+    });
+
+    it('Passes 7th test set', function(done){
+      debt.dispo(data[6]).then(function(results){
+        var values = {};
+        _.each(results._1, function(el, ind, list){ values[ind]= Math.round(el.value * ROUND_PRECISION) / ROUND_PRECISION;});
+        assert(_.isMatch(values, expectations[6]));
+        done();
+      }).onReject(done);
+    });
+
+    it('Passes 8th test set', function(done){
+      debt.dispo(data[7]).then(function(results){
+        var values = {};
+        _.each(results._1, function(el, ind, list){ values[ind]= Math.round(el.value * ROUND_PRECISION) / ROUND_PRECISION;});
+        assert(_.isMatch(values, expectations[7]));
+        done();
+      }).onReject(done);
+    });
+
+    it('Passes 9th test set', function(done){
+      debt.dispo(data[8]).then(function(results){
+        var values = {};
+        _.each(results._1, function(el, ind, list){ values[ind]= Math.round(el.value * ROUND_PRECISION) / ROUND_PRECISION;});
+        assert(_.isMatch(values, expectations[8]));
+        done();
+      }).onReject(done);
+    });
+
+    it('Passes 10th test set', function(done){
+      debt.dispo(data[9]).then(function(results){
+        var values = {};
+        _.each(results._1, function(el, ind, list){ values[ind]= Math.round(el.value * ROUND_PRECISION) / ROUND_PRECISION;});
+        assert(_.isMatch(values, expectations[9]));
+        done();
+      }).onReject(done);
+    });
+
+    it('Passes 11th test set', function(done){
+      debt.dispo(data[10]).then(function(results){
+        var values = {};
+        _.each(results._1, function(el, ind, list){ values[ind]= Math.round(el.value * ROUND_PRECISION) / ROUND_PRECISION;});
+        assert(_.isMatch(values, expectations[10]));
+        done();
+      }).onReject(done);
+    });
+
+    it('Passes 12th test set', function(done){
+      debt.dispo(data[11]).then(function(results){
+        var values = {};
+        _.each(results._1, function(el, ind, list){ values[ind]= Math.round(el.value * ROUND_PRECISION) / ROUND_PRECISION;});
+        assert(_.isMatch(values, expectations[11]));
+        done();
+      }).onReject(done);
+    });
+
+    it('Passes 13th test set', function(done){
+      debt.dispo(data[12]).then(function(results){
+        var values = {};
+        _.each(results._1, function(el, ind, list){ values[ind]= Math.round(el.value * ROUND_PRECISION) / ROUND_PRECISION;});
+        assert(_.isMatch(values, expectations[12]));
+        done();
+      }).onReject(done);
+    });
+
+    it('Passes 14th test set', function(done){
+      debt.dispo(data[13]).then(function(results){
+        var values = {};
+        _.each(results._1, function(el, ind, list){ values[ind]= Math.round(el.value * ROUND_PRECISION) / ROUND_PRECISION;});
+        assert(_.isMatch(values, expectations[13]));
+        done();
+      }).onReject(done);
+    });
+
+    it('Passes 15th test set', function(done){
+      debt.dispo(data[14]).then(function(results){
+        var values = {};
+        _.each(results._1, function(el, ind, list){ values[ind]= Math.round(el.value * ROUND_PRECISION) / ROUND_PRECISION;});
+        assert(_.isMatch(values, expectations[14]));
+        done();
+      }).onReject(done);
+    });
+
+    it('Passes 16th test set', function(done){
+      debt.dispo(data[15]).then(function(results){
+        var values = {};
+        _.each(results._1, function(el, ind, list){ values[ind]= Math.round(el.value * ROUND_PRECISION) / ROUND_PRECISION;});
+        assert(_.isMatch(values, expectations[15]));
+        done();
+      }).onReject(done);
+    });
+
+    it('Passes 17th test set', function(done){
+      debt.dispo(data[16]).then(function(results){
+        var values = {};
+        _.each(results._1, function(el, ind, list){ values[ind]= Math.round(el.value * ROUND_PRECISION) / ROUND_PRECISION;});
+        assert(_.isMatch(values, expectations[16]));
+        done();
+      }).onReject(done);
+    });
+
+    it('Passes 18th test set', function(done){
+      debt.dispo(data[17]).then(function(results){
+        var values = {};
+        _.each(results._1, function(el, ind, list){ values[ind]= Math.round(el.value * ROUND_PRECISION) / ROUND_PRECISION;});
+        assert(_.isMatch(values, expectations[17]));
+        done();
+      }).onReject(done);
+    });
+
+    it('Passes 19th test set', function(done){
+      debt.dispo(data[18]).then(function(results){
+        var values = {};
+        _.each(results._1, function(el, ind, list){ values[ind]= Math.round(el.value * ROUND_PRECISION) / ROUND_PRECISION;});
+        assert(_.isMatch(values, expectations[18]));
+        done();
+      }).onReject(done);
+    });
+
+    it('Passes 20th test set', function(done){
+      debt.dispo(data[19]).then(function(results){
         var values = {};
         _.each(results._1, function(el, ind, list){ values[ind]= Math.round(el.value * ROUND_PRECISION) / ROUND_PRECISION;});
         assert(_.isMatch(values, expectations[19]));
