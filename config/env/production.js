a8ef49a1c268ab244cbfdf3039d4740cc8a42fd6
Benjamin Tanz <btanz@gmx.de>
2015-09-17 08:52:06 +0200

    adjusted database connection settings

--- config/env/production.js
+++ config/env/production.js
@@ -2,11 +2,19 @@
  * PRODUCTION environment config
  */
 
+var dbSettings = {
+  "USER": "",
+  "PASS": "",
+  "HOST": "localhost",
+  "PORT": "27017",
+  "DATABASE": "simplyfi"
+};
+
+
 module.exports = {
 
   /** MongoDB connection URI */
-  // todo: adjust for production mode
-  db: 'mongodb://localhost/simplyfi',
+  db: "mongodb://" + dbSettings.USER + ":" + dbSettings.PASS + "@" + dbSettings.HOST + ":"+ dbSettings.PORT + "/"+ dbSettings.DATABASE,
 
   /** response compression */
   compression: true
